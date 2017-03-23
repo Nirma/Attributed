01d34bb5cf3be77d83036cb5f147dedcc91d3407
Nicholas Maccharoli <nmaccharoli@gmail.com>
2017-03-23 19:58:10 +0900

    Introduce Swiftlint and fix whitespace issues

--- Attributed/Operators.swift
+++ Attributed/Operators.swift
@@ -15,82 +15,80 @@ public func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributed
     return NSAttributedString(attributedString: result)
 }
 
-
 // NSParagraphStyle Addition
 
 public func + (lhs: NSParagraphStyle, rhs: NSParagraphStyle) -> NSParagraphStyle {
     let defaultParagraph = NSParagraphStyle.default
     let combinedAttributes = lhs.mutableCopy() as! NSMutableParagraphStyle
-    
+
     if rhs.lineSpacing != defaultParagraph.lineSpacing {
         combinedAttributes.lineSpacing = rhs.lineSpacing
     }
-    
+
     if rhs.paragraphSpacing != defaultParagraph.paragraphSpacing {
         combinedAttributes.paragraphSpacing = rhs.paragraphSpacing
     }
-    
+
     if rhs.alignment != defaultParagraph.alignment {
         combinedAttributes.alignment = rhs.alignment
     }
-    
+
     if rhs.firstLineHeadIndent != defaultParagraph.firstLineHeadIndent {
         combinedAttributes.firstLineHeadIndent = rhs.firstLineHeadIndent
     }
-    
+
     if rhs.headIndent != defaultParagraph.headIndent {
         combinedAttributes.headIndent = rhs.headIndent
     }
-    
+
     if rhs.tailIndent != defaultParagraph.tailIndent {
         combinedAttributes.tailIndent = rhs.tailIndent
     }
-    
+
     if rhs.lineBreakMode != defaultParagraph.lineBreakMode {
         combinedAttributes.lineBreakMode = rhs.lineBreakMode
     }
-    
+
     if rhs.minimumLineHeight != defaultParagraph.minimumLineHeight {
         combinedAttributes.minimumLineHeight = rhs.minimumLineHeight
     }
-    
+
     if rhs.maximumLineHeight != defaultParagraph.maximumLineHeight {
         combinedAttributes.maximumLineHeight = rhs.maximumLineHeight
     }
-    
+
     if rhs.baseWritingDirection != defaultParagraph.baseWritingDirection {
         combinedAttributes.baseWritingDirection = rhs.baseWritingDirection
     }
-    
+
     if rhs.lineHeightMultiple != defaultParagraph.lineHeightMultiple {
         combinedAttributes.lineHeightMultiple = rhs.lineHeightMultiple
     }
-    
+
     if rhs.paragraphSpacingBefore != defaultParagraph.paragraphSpacingBefore {
         combinedAttributes.paragraphSpacingBefore = rhs.paragraphSpacingBefore
     }
-    
+
     if rhs.hyphenationFactor != defaultParagraph.hyphenationFactor {
         combinedAttributes.hyphenationFactor = rhs.hyphenationFactor
     }
-    
+
     if rhs.tabStops != defaultParagraph.tabStops {
         combinedAttributes.tabStops = rhs.tabStops
     }
-    
+
     if rhs.defaultTabInterval != defaultParagraph.defaultTabInterval {
         combinedAttributes.defaultTabInterval = rhs.defaultTabInterval
     }
-    
+
     if #available(iOS 9.0, *) {
         if rhs.allowsDefaultTighteningForTruncation != defaultParagraph.allowsDefaultTighteningForTruncation {
             combinedAttributes.allowsDefaultTighteningForTruncation = rhs.allowsDefaultTighteningForTruncation
         }
     }
-    
+
     return combinedAttributes
 }
-
 
 // MARK: Attributes addition
 
@@ -99,20 +97,20 @@ public func + (lhs: Attributes, rhs: Attributes) -> Attributes {
     for (key, value) in rhs.dictionary {
         combined[key] = value
     }
-    
+
     let combinedParagraphStyle: NSParagraphStyle?
     let lhsParagraphStyle = lhs.dictionary[NSParagraphStyleAttributeName] as? NSParagraphStyle
     let rhsParagraphStyle = rhs.dictionary[NSParagraphStyleAttributeName] as? NSParagraphStyle
-    
+
     if let lhsParagraphStyle = lhsParagraphStyle, let rhsParagraphStyle = rhsParagraphStyle {
         combinedParagraphStyle = lhsParagraphStyle + rhsParagraphStyle
     } else {
         combinedParagraphStyle = lhsParagraphStyle ?? rhsParagraphStyle
     }
-    
+
     if let paragraphStyle = combinedParagraphStyle {
         combined[NSParagraphStyleAttributeName] = paragraphStyle
     }
-    
+
     return Attributes(dictionary: combined)
 }
--- Attributed/String+Attributed.swift
+++ Attributed/String+Attributed.swift
@@ -10,53 +10,53 @@ import Foundation
 import UIKit
 
 public struct Attributes {
-    
+
     let dictionary: [String: Any]
-    
+
     public init() {
         dictionary = [:]
     }
-    
+
     public init(_ attributesBlock: (Attributes) -> Attributes) {
         self = attributesBlock(Attributes())
     }
-    
+
     internal init(dictionary: [String: Any]) {
         self.dictionary = dictionary
     }
-    
+
     public func font(_ font: UIFont) -> Attributes {
         return self + Attributes(dictionary: [NSFontAttributeName: font])
     }
-    
+
     public func kerning(_ kerning: Double) -> Attributes {
         return self + Attributes(dictionary: [NSKernAttributeName: NSNumber(floatLiteral: kerning)])
     }
-    
+
     public func strikeThroughStyle(_ strikeThroughStyle: NSUnderlineStyle) -> Attributes {
         return self + Attributes(dictionary: [NSStrikethroughStyleAttributeName: strikeThroughStyle.rawValue])
     }
-    
+
     public func underlineStyle(_ underlineStyle: NSUnderlineStyle) -> Attributes {
         return self + Attributes(dictionary: [NSUnderlineStyleAttributeName: underlineStyle.rawValue])
     }
-    
+
     public func strokeColor(_ strokeColor: UIColor) -> Attributes {
         return self + Attributes(dictionary: [NSStrokeColorAttributeName: strokeColor])
     }
-    
+
     public func strokeWidth(_ strokewidth: Double) -> Attributes {
         return self + Attributes(dictionary: [NSStrokeWidthAttributeName: NSNumber(floatLiteral: strokewidth)])
     }
-    
+
     public func foreground(color: UIColor) -> Attributes {
         return self + Attributes(dictionary: [NSForegroundColorAttributeName: color])
     }
-    
+
     public func background(color: UIColor) -> Attributes {
         return self + Attributes(dictionary: [NSBackgroundColorAttributeName: color])
     }
-    
+
     public func paragraphStyle(_ paragraphStyle: NSParagraphStyle) -> Attributes {
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
@@ -70,83 +70,82 @@ public struct Attributes {
     }
 }
 
-
 // MARK: NSParagraphStyle related
 
 extension Attributes {
-    
+
     public func lineSpacing(_ lineSpacing: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.lineSpacing = lineSpacing
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func paragraphSpacing(_ paragraphSpacing: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.paragraphSpacing =  paragraphSpacing
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func alignment(_ alignment: NSTextAlignment) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.alignment = alignment
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func firstLineHeadIndent(_ firstLineHeadIndent: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.firstLineHeadIndent = firstLineHeadIndent
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func headIndent(_ headIndent: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.headIndent = headIndent
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func tailIndent(_ tailIndent: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.tailIndent = tailIndent
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func lineBreakMode(_ lineBreakMode: NSLineBreakMode) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.lineBreakMode = lineBreakMode
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func minimumLineHeight(_ minimumLineHeight: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.minimumLineHeight = minimumLineHeight
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func maximumLineHeight(_ maximumLineHeight: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.maximumLineHeight = maximumLineHeight
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func baseWritingDirection(_ baseWritingDirection: NSWritingDirection) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.baseWritingDirection = baseWritingDirection
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func lineHeightMultiple(_ lineHeightMultiple: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.lineHeightMultiple = lineHeightMultiple
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func paragraphSpacingBefore(_ paragraphSpacingBefore: CGFloat) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.paragraphSpacingBefore = paragraphSpacingBefore
         return self + Attributes(dictionary: [NSParagraphStyleAttributeName: paragraphStyle])
     }
-    
+
     public func hyphenationFactor(_ hyphenationFactor: Float) -> Attributes {
         let paragraphStyle = (dictionary[NSParagraphStyleAttributeName] ?? NSMutableParagraphStyle.default.mutableCopy()) as! NSMutableParagraphStyle
         paragraphStyle.hyphenationFactor = hyphenationFactor
@@ -154,22 +153,20 @@ extension Attributes {
     }
 }
 
-
 // MARK: Primary Extension
 
 extension String {
-    
+
     public func attributed(with attributes: Attributes) -> NSAttributedString {
         let attributes = attributes.dictionary
         return NSAttributedString(string: self, attributes: attributes)
     }
-    
+
     public func attributed(_ attributeBlock: (Attributes) -> (Attributes)) -> NSAttributedString {
         let attributes = attributeBlock(Attributes())
         return attributed(with: attributes)
     }
 }
-
 
 extension NSString {
 
@@ -183,7 +180,6 @@ extension NSString {
 
 }
 
-
 extension NSAttributedString {
 
     public func modified(with attributes: Attributes, for range: NSRange) -> NSAttributedString {
@@ -193,7 +189,6 @@ extension NSAttributedString {
     }
 
 }
-
 
 extension NSMutableAttributedString {
 
